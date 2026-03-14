49c621d7884468d33a0760df75bc7bd77bae48e8
rudrabhavsar7 <23dcs012@charusat.edu.in>
2026-03-14 16:03:46 +0530

    Add ingestion and preprocessing notebooks for alert data processing

---
+++ etl/silver/silver.sql
@@ -0,0 +1,22 @@
+-- Active: 1773470111546@@127.0.0.1@5432@kenexaihackathon
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE TABLE IF NOT EXISTS silver.alerts (
+    alert_id TEXT PRIMARY KEY,
+
+    source_system VARCHAR(50),
+
+    device_name VARCHAR(150),
+    device_identifier TEXT,
+
+    organization_name TEXT,
+    network_name TEXT,
+
+    alert_type VARCHAR(150),
+    severity VARCHAR(50),
+
+    alert_message TEXT,
+
+    occurred_at TIMESTAMP,
+
+    ingestion_time TIMESTAMP
+);
